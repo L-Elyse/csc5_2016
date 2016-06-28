5972dce993a18fcaf96c0ba4c3041e138479e526
L-Elyse <lguimont@student.rcc.edu>
2016-06-27 21:50:55 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

---
+++ Homework/Assignment 2/Gaddis_8thEd_Chap3_Prob3_TestAverage/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Laurie Guimont
+ * Created on June 25, 2016, 5:10 PM
+ * Purpose: Template
+ */
+
+//System Libraries
+#include <iostream> //Input/Output Stream Library
+using namespace std;//Iostream uses the standard namespace
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare variables, no doubles
+    
+    
+    //Input data
+    
+    
+    //Process data
+    
+    
+    //Output data
+    
+    
+    //Exit Stage Right!
+    return 0;
+}
