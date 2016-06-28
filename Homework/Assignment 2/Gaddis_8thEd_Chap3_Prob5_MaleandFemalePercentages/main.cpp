118368d113db9d72d3c21bef8975ff66060e50a7
L-Elyse <lguimont@student.rcc.edu>
2016-06-27 23:47:17 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

--- Homework/Assignment 2/Gaddis_8thEd_Chap3_Prob5_MaleandFemalePercentages/main.cpp
+++ Homework/Assignment 2/Gaddis_8thEd_Chap3_Prob5_MaleandFemalePercentages/main.cpp
@@ -1,13 +1,13 @@
 /* 
  * File:   main.cpp
  * Author: Laurie Guimont
- * Created on June 27, 2016, 9:35 PM
- * Purpose: Test Average
+ * Created on June 27, 2016, 11:33 PM
+ * Purpose: Male & Female Percentages in a class
  */
 
 //System Libraries
 #include <iostream> //Input/Output Stream Library
-#include <iomanip> //Format Library
+#include <iomanip>  //Format Library
 using namespace std;//Iostream uses the standard namespace
 
 //User Libraries
@@ -19,28 +19,25 @@ using namespace std;//Iostream uses the standard namespace
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare variables, no doubles
-    float score1, score2, score3, score4, score5; //Test scores
-    float average;
-    
+    float males, females; //Number of males and females in the class
+    float total;          //Total students in the class
+    float mperc, fperc;   //Percent representation of males and females
     
     //Input data
-    cout<<"Please enter your first test score."<<endl;
-    cin>>score1;
-    cout<<"Please enter your second test score."<<endl;
-    cin>>score2;
-    cout<<"Please enter your third test score."<<endl;
-    cin>>score3;
-    cout<<"Please enter your fourth test score."<<endl;
-    cin>>score4;
-    cout<<"Please enter your fifth test score."<<endl;
-    cin>>score5;
+    cout<<"How many males are in your class?"<<endl;
+    cin>>males;
+    cout<<"How many females are in your class?"<<endl;
+    cin>>females;
     
     //Process data
-    average=(score1+score2+score3+score4+score5)/5;
+    total=males+females;
+    mperc=males/total*100;
+    fperc=females/total*100;
     
     //Output data
-    cout<<fixed<<setprecision(1)<<showpoint;
-    cout<<"Your test average is = "<<average<<endl;
+    cout<<setprecision(2);
+    cout<<"Total percentage of males in the class   = "<<mperc<<"%"<<endl;
+    cout<<"Total percentage of females in the class = "<<fperc<<"%"<<endl;
     
     //Exit Stage Right!
     return 0;
