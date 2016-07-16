eed118bf172f378533349c7491fe446ac0191be2
L-Elyse <lguimont@student.rcc.edu>
2016-07-16 01:34:54 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

--- Project/Project_v1/main.cpp
+++ Project/Project_v1/main.cpp
@@ -1,12 +1,14 @@
 /* 
  * File:   main.cpp
  * Author: Laurie Guimont
- * Created on June 20, 2016, 12:27 PM
- * Purpose: First Program
+ * Created on July 10, 2016, 8:55 PM
+ * Purpose: Random Number Guessing Game (Enhancement)
  */
 
 //System Libraries
-#include <iostream>//Input/Output Stream Library
+#include <iostream> //Input/Output Stream Library
+#include <ctime>    //Unique Seed Value Library
+#include <cstdlib>  //Random Value Library
 using namespace std;//Iostream uses the standard namespace
 
 //User Libraries
@@ -18,14 +20,52 @@ using namespace std;//Iostream uses the standard namespace
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare variables, no doubles
+    char choice;
+    int nwins=0,nlosses=0,nwars=0;
+    char number=time(0);  //Random numbers to be added together set to present time
+    int warnum;          //Number of cards to throw before flipping in war
+    const int MIN=1;     //Minimum value to choose from--TESTING VALUE ONLY--CHANGE!
+    const int MAX=14;    //Maximum value to choose from--TESTING VALUE ONLY--CHANGE!
     
-    //Input data
+    //Input Data
+    cout<<"The name of the game? WAR!"<<endl;
+    cout<<"Pick a card the you would like to play"<<endl;
+    cin>>choice;
     
-    //Process data
+    //Input Validation--will be taken care of with a switch!
+//    if(choice<'1'||choice>'9'){
+//        //Get Input Again!
+//        cout<<"Please enter an integer between 1 and 20!"<<endl;
+//        cin>>choice;
+//    }
+          
+    //Set Random Number Seed 
+    srand(number);
+    number = (rand() % (MAX - MIN + 1) + MIN);
     
-    //Output data
-    cout<<"Hello World"<<endl;
+    //Processing Choice--loop here!
+    if(choice>number){
+        nwins+=1;
+        cout<<"You won!"<<endl;
+        }
+    else if(choice<number){
+        nlosses+=1;
+        cout<<"Sorry. You lost."<<endl;
+        }
+    else{
+        nwars+=1;
+        cout<<"War!!! How many cards do you want to throw down ";
+        cout<<"before you flip one?"<<endl;
+        cin>>warnum;       
+    }
     
+    //Game Stats
+    cout<<"Computer's card: "<<number<<endl;
+    cout<<endl;
+    cout<<"Wins:   "<<nwins<<endl;
+    cout<<"Losses: "<<nlosses<<endl;
+    cout<<"Wars:   "<<nwars<<endl;
+         
     //Exit Stage Right!
     return 0;
 }
