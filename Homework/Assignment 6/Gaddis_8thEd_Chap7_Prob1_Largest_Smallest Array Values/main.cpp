d57514f0767f6da39d942c24a8090f1c9c4d54db
L-Elyse <lguimont@student.rcc.edu>
2016-07-22 20:43:30 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

--- Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob1_Largest_Smallest Array Values/main.cpp
+++ Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob1_Largest_Smallest Array Values/main.cpp
@@ -14,32 +14,39 @@ using namespace std;//Iostream uses the standard namespace
 //Global Constants
 
 //Function Prototypes
-void getNum(int [],int);
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare variables, no doubles
+    const int SIZE=10;
+    int number[SIZE];
+    int highest,lowest;
+    int count;
 
     //Input data
+    for(count=0;count<SIZE;count++){
+        cout<<"Please enter number "<<count+1<<endl;
+        cin>>number[count];
+    }
     
-    //Process data
+    //Display & Process data
+    highest=number[0];
+    for(count=1;count<SIZE;count++){
+        if (number[count]>highest) 
+            highest=number[count];
+    }
+    
+    lowest=number[0];
+    for(count=1;count<SIZE;count++){
+        if (number[count]<lowest) 
+            lowest=number[count];
+    }
     
     //Output data
-    cout<<
+    cout<<endl;
+    cout<<"Highest Number: "<<highest<<endl;
+    cout<<"Lowest Number : "<<lowest<<endl;
     
     //Exit Stage Right!
     return 0;
-}
-
-void getNum(int input[],int SIZE){
-    //Declare variables, no doubles
-    const int SIZE=10;
-    int input[SIZE];
-    
-    //Input data
-    for(int count=0;cout<SIZE-1;count++){
-        cout<<"Please enter number "<<count+1<<endl;
-        cin>>input[SIZE];    
-    }
-    return;
 }
