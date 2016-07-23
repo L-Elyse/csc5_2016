c1aafc1928f5fa3b0efa35e0acb98d8840fc7990
L-Elyse <lguimont@student.rcc.edu>
2016-07-22 23:59:46 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

--- Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob2_Rainfall Statistics/main.cpp
+++ Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob2_Rainfall Statistics/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Laurie Guimont
  * Created on July 22, 2016, 8:50 PM
- * Purpose: First Program
+ * Purpose: Calculate Rainfall
  */
 
 //System Libraries
@@ -25,17 +25,24 @@ int main(int argc, char** argv) {
     float rain[SIZE];
     float total=0;
     float highest,lowest;
-    string hmonth;
     int count;
     
     //Input data
     for(count=0;count<SIZE;count++){
         cout<<"What was the total rainfall for "<<month[count]<<"?"<<endl;
         cin>>rain[count];
+        
+        //Input Validation
+        while(rain[count]<0){
+            cout<<"Error! Please enter only positive numbers"<<endl;
+            cin>>rain[count];
+        }
+        
         total+=rain[count];
     }
     
     //Process & Output Data
+    //Highest Amount of Rainfall
     highest=rain[0];
     for(count=1;count<SIZE;count++){
         if(rain[count]>highest){
@@ -43,16 +50,31 @@ int main(int argc, char** argv) {
         } 
     } 
     
-    if(highest==rain[4])cout<<month[4];
-    if()
+    for(count=1;count<SIZE;count++){
+        if(highest==rain[count]){
+            cout<<month[count]<<" had the highest amount of rainfall, with ";
+            cout<<highest<<" inches."<<endl;
+        }
+    }
+    //Lowest Amount of Rainfall
+    lowest=rain[0];
+    for(count=1;count<SIZE;count++){
+        if(rain[count]<lowest){
+            lowest=rain[count];
+        } 
+    } 
     
+    for(count=1;count<SIZE;count++){
+        if(lowest==rain[count]){
+            cout<<month[count]<<" had the lowest amount of rainfall, with ";
+            cout<<lowest<<" inches."<<endl;
+        }
+    }
+    //Total and Average Rainfall       
     cout<<endl;
     cout<<"The total rainfall for the year was "<<total<<" inches."<<endl;
     cout<<"Average rainfall: "<<total/SIZE<<" inches."<<endl;
-    cout<<hmonth;
-    
-    
-    
+      
     //Exit Stage Right!
     return 0;
 }
